1f3af565047ef4709f5937bd4737688fe28daa49
Christoph Rist <rist.christoph@gmail.com>
2016-11-19 16:54:28 +0100

    Fix plug combinations, refactor settings generation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from itertools import permutations
+from itertools import permutations, combinations
 from joblib import Parallel, delayed
 from multiprocessing import cpu_count
 
@@ -13,40 +13,59 @@ __copyright__ = "Copyright 2016, Christoph Rist"
 __license__ = "MIT"
 
 CIPHERTEXT = 'HOOJILEQKCYJN'
-plugboard = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-                 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z']
+NUMBER_RANGE = list(range(26))
 
 
-def get_plug_string(l):
-    ret = ''
-    for i in range(0, 20, 2):
-        ret += l[i]
-        ret += l[i+1]
-        ret += " "
+def generate_plug_settings():
 
-    return ret
+    def generate_pairs_from_set(l, r):
+        for i in range(1, len(l)):
+            rn = r + l[0] + l[i] + " "
+            ln = [l[j] for j in range(1, len(l)) if j is not i]
+            if ln:
+                yield from generate_pairs_from_set(ln, rn)
+            else:
+                yield rn
+
+    for comb in combinations(ALPHABET, 20):
+        plug = ""
+        yield from generate_pairs_from_set(comb, plug)
+
+
+def generate_rotor_positions():
+    for s1 in ALPHABET:
+        for s2 in ALPHABET:
+            for s3 in ALPHABET:
+                yield s1 + s2 + s3
+
+
+def generate_ring_settings():
+    for r1 in NUMBER_RANGE:
+        for r2 in NUMBER_RANGE:
+            for r3 in NUMBER_RANGE:
+                yield [r1, r2, r3]
 
 
 def run_enigma(d):
-    counter = 0
-    for plug_perm in permutations(plugboard, 20):
-        counter += 1
-        if counter % 1000000 == 0:
-            print(str(counter/5.6012703e21)+"%")
+    machine = EnigmaMachine.from_key_sheet(
+        rotors=d["rotor"],
+        reflector=d["ref"],
+        ring_settings=d["ring"],
+        plugboard_settings=d["plug"])
 
-        machine = EnigmaMachine.from_key_sheet(
-            rotors=d["rotor"],
-            reflector=d["ref"],
-            ring_settings=d["ring"],
-            plugboard_settings=get_plug_string(plug_perm))
+    print("Created enigma from settings: ")
+    print(d)
 
-        machine.set_display(d["start"])
-
+    for r in generate_rotor_positions():
+        machine.set_display(r)
         output = machine.process_text(CIPHERTEXT)
         if output == "BLETCHLEYPARK":
             print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
             print(d)
-            print("PLUGS: ", plug_perm)
+            print("ROTOR POSITION: ", r)
             print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
             raise RuntimeError("Solution found.")
 
@@ -54,42 +73,20 @@ def run_enigma(d):
 def gen_setting():
     rotors = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII']
     reflectors = ['B'] #, 'C'] this is known to be reflector B!
-    ring_setting = list(range(26))
 
-    for r1 in ring_setting:
-        for r2 in ring_setting:
-            for r3 in ring_setting:
-                for refl in reflectors:
-                    for s1 in plugboard:
-                        for s2 in plugboard:
-                            for s3 in plugboard:
-                                for rot_perm in permutations(rotors, 3):
-                                    yield {"rotor": rot_perm, "ref": refl, "ring": [r1, r2, r3], "start": s1 + s2 + s3}
-                                    print("Checking rotor:", rot_perm)
+    for r in generate_ring_settings():
+        for refl in reflectors:
+            for rot_perm in permutations(rotors, 3):
+                for plug_perm in generate_plug_settings():
+                    yield {"rotor": rot_perm, "ref": refl, "ring": r, "plug": plug_perm}
 
 
 def main():
 
-    assert(len(plugboard) == 26)
+    assert(len(ALPHABET) == 26)
 
     num_cores = cpu_count()
-
     Parallel(n_jobs=num_cores)(delayed(run_enigma)(i) for i in gen_setting())
-
-
-    # set machine initial starting position
-    #machine.set_display('WXC')
-
-    # decrypt the message key
-    #msg_key = machine.process_text('KCH')
-
-    # decrypt the cipher text with the unencrypted message key
-    #machine.set_display(msg_key)
-
-    #ciphertext = 'NIBLFMYMLLUFWCASCSSNVHAZ'
-    #plaintext = machine.process_text(ciphertext)
-
-    #print(plaintext)
 
 
 if __name__ == "__main__":
