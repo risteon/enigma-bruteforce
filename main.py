c3c2c508b7477f3532882e755fbb16db1239df4d
Christoph Rist <rist.christoph@gmail.com>
2016-11-19 20:08:03 +0100

    Handle the async discovery of a solution

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 from itertools import permutations, combinations
-from joblib import Parallel, delayed
-from multiprocessing import cpu_count
+import string
 
+from multiprocessing import cpu_count, Event, Queue, Pool
+import time
+import argparse
 
 from enigma.machine import EnigmaMachine
 
@@ -12,67 +14,42 @@ __author__ = "Christoph Rist"
 __copyright__ = "Copyright 2016, Christoph Rist"
 __license__ = "MIT"
 
-CIPHERTEXT = 'HOOJILEQKCYJN'
+CIPHERTEXT = ''
+PLAINTEXT = ''
 
-ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-            'U', 'V', 'W', 'X', 'Y', 'Z']
+ALPHABET = list(string.ascii_uppercase)
 NUMBER_RANGE = list(range(26))
 
-
-def generate_plug_settings():
-
-    def generate_pairs_from_set(l, r):
-        for i in range(1, len(l)):
-            rn = r + l[0] + l[i] + " "
-            ln = [l[j] for j in range(1, len(l)) if j is not i]
-            if ln:
-                yield from generate_pairs_from_set(ln, rn)
-            else:
-                yield rn
-
-    for comb in combinations(ALPHABET, 20):
-        plug = ""
-        yield from generate_pairs_from_set(comb, plug)
-
-
-def generate_rotor_positions():
-    for s1 in ALPHABET:
-        for s2 in ALPHABET:
-            for s3 in ALPHABET:
-                yield s1 + s2 + s3
-
-
-def generate_ring_settings():
-    for r1 in NUMBER_RANGE:
-        for r2 in NUMBER_RANGE:
-            for r3 in NUMBER_RANGE:
-                yield [r1, r2, r3]
-
-
-def run_enigma(d):
-    machine = EnigmaMachine.from_key_sheet(
-        rotors=d["rotor"],
-        reflector=d["ref"],
-        ring_settings=d["ring"],
-        plugboard_settings=d["plug"])
-
-    print("Created enigma from settings: ")
-    print(d)
-
-    for r in generate_rotor_positions():
-        machine.set_display(r)
-        output = machine.process_text(CIPHERTEXT)
-        if output == "BLETCHLEYPARK":
-            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-            print(d)
-            print("ROTOR POSITION: ", r)
-            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-            raise RuntimeError("Solution found.")
+stop_event = Event()
+max_queue_size = 4*cpu_count()
+data_queue = Queue(max_queue_size)
+solution_queue = Queue(cpu_count())
 
 
 def gen_setting():
+    def generate_plug_settings():
+
+        def generate_pairs_from_set(l, p_comb):
+            for i in range(1, len(l)):
+                rn = p_comb + l[0] + l[i] + " "
+                ln = [l[j] for j in range(1, len(l)) if j is not i]
+                if ln:
+                    yield from generate_pairs_from_set(ln, rn)
+                else:
+                    yield rn
+
+        for comb in combinations(ALPHABET, 20):
+            plug = ""
+            yield from generate_pairs_from_set(comb, plug)
+
+    def generate_ring_settings():
+        for r1 in NUMBER_RANGE:
+            for r2 in NUMBER_RANGE:
+                for r3 in NUMBER_RANGE:
+                    yield [r1, r2, r3]
+
     rotors = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII']
-    reflectors = ['B'] #, 'C'] this is known to be reflector B!
+    reflectors = ['B', 'C']
 
     for r in generate_ring_settings():
         for refl in reflectors:
@@ -81,16 +58,99 @@ def gen_setting():
                     yield {"rotor": rot_perm, "ref": refl, "ring": r, "plug": plug_perm}
 
 
-def main():
+def combinations_task():
+    for setting in gen_setting():
+        while data_queue.qsize() >= max_queue_size:
+            time.sleep(0.2)
+            if stop_event.is_set():
+                break
+        data_queue.put(setting)
 
-    assert(len(ALPHABET) == 26)
+        if stop_event.is_set():
+            break
+
+
+def run_enigma_task(num):
+    print("Started worker #{}".format(num))
+    while not stop_event.is_set():
+        while data_queue.empty():
+            if stop_event.is_set():
+                return
+            time.sleep(0.05)
+        d = data_queue.get()
+
+        def generate_rotor_positions():
+            for s1 in ALPHABET:
+                for s2 in ALPHABET:
+                    for s3 in ALPHABET:
+                        yield s1 + s2 + s3
+
+        machine = EnigmaMachine.from_key_sheet(
+            rotors=d["rotor"],
+            reflector=d["ref"],
+            ring_settings=d["ring"],
+            plugboard_settings=d["plug"])
+
+        print("Created enigma from settings: ")
+        print(d)
+
+        for r in generate_rotor_positions():
+            machine.set_display(r)
+
+            for cipher, char in zip(CIPHERTEXT, PLAINTEXT):
+                output = machine.process_text(cipher)
+                if output != char:
+                    break
+            else:
+                d["display"] = r
+                solution_queue.put(d)
+                stop_event.set()
+                return
+
+
+def main():
+    parser = argparse.ArgumentParser("Try to find enigma settings when cipher and plain text are known.")
+    parser.add_argument('--ciphertext', metavar='STRING', required=True)
+    parser.add_argument('--plaintext', metavar='STRING', required=True)
+    args = parser.parse_args()
+
+    global CIPHERTEXT
+    global PLAINTEXT
+    CIPHERTEXT = args.ciphertext.upper()
+    PLAINTEXT = args.plaintext.upper()
+    # sanity check ciphertext and plaintext
+    if not CIPHERTEXT.isalpha():
+        print("Error: Ciphertext can only contain letters.")
+        return
+    if not PLAINTEXT.isalpha():
+        print("Error: Plaintext can only contain letters.")
+        return
+    if len(CIPHERTEXT) != len(PLAINTEXT):
+        print("Error: Ciphertext and plaintext have to be equal in length.")
+        return
+    for a, b in zip(PLAINTEXT, CIPHERTEXT):
+        if a == b:
+            print("Error: No letter is encrypted to itself. There will be no solution.")
+            return
 
     num_cores = cpu_count()
-    Parallel(n_jobs=num_cores)(delayed(run_enigma)(i) for i in gen_setting())
+    pool = Pool(num_cores)
+    r = pool.map_async(run_enigma_task, [i for i in range(num_cores)])
+    # generate combinations
+    combinations_task()
+
+    pool.close()
+    pool.join()
+
+    if stop_event.is_set():
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        print("Solution found! Encrypt {} to {}".format(PLAINTEXT, CIPHERTEXT))
+        while not solution_queue.empty():
+            print(solution_queue.get())
+        print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    else:
+        print("No solution found")
 
 
 if __name__ == "__main__":
     main()
-
-
-
